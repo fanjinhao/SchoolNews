abd30ace4ca9273b53153339f372c50e2a4f0a22
Finhoo <fjinhao@qq.com>
2017-11-16 21:56:31 +0800

    分享按钮，未实现

--- app/src/main/java/com/fayne/android/schoolnews/activity/NewsInfoActivity.java
+++ app/src/main/java/com/fayne/android/schoolnews/activity/NewsInfoActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.text.TextUtils;
 import android.view.View;
@@ -50,6 +52,13 @@ public class NewsInfoActivity extends BaseActivity {
             }
         });
         new LoadDataTask().execute();
+        FloatingActionButton fab = findViewById(R.id.fab_share);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     class LoadDataTask extends AsyncTask<Void, Void, String> {
