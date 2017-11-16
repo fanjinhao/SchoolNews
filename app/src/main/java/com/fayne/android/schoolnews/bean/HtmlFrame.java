232032dfa81e70714b118b18e91f72ce874c0359
Finhoo <fjinhao@qq.com>
2017-11-17 00:49:48 +0800

    测试模块

--- app/src/main/java/com/fayne/android/schoolnews/bean/HtmlFrame.java
+++ app/src/main/java/com/fayne/android/schoolnews/bean/HtmlFrame.java
@@ -9,5 +9,5 @@ public class HtmlFrame {
     public static final String FRAME = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></head>" +
             "<div class=\"wrapper\"><h3>%s</h3>" +
             "<div class=\"info\"><span>%s</span></div><br />" +
-            "<div class=\"text\">%s</div>";
+            "<div class=\"text\">%s</div><br><br>";
 }
